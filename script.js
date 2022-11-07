873c79fdabd8f42609d13f2300ff068da519f8cb
Kiril Vandzsurak <kiril.wandzurak@gmail.com>
2022-11-07 16:58:17 +0100

    Showing API's content

--- script.js
+++ script.js
@@ -18,11 +18,10 @@ fetch(
     for (data of pinkfloyd.data) {
       getContainer.innerHTML += `<div class="col-3">
       <div class="card">
-  <img src="..." class="card-img-top" alt="...">
+  <img src="${data.album.cover}" class="card-img-top" alt="...">
   <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
+    <h5 class="card-title">${data.artist.name}</h5>
+    <p class="card-text">Title: ${data.title}</p>
   </div>
 </div>
       </div>`;
