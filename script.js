d50c97ca139e4902076a4fbd2457a995a75f73fe
Kiril Vandzsurak <kiril.wandzurak@gmail.com>
2022-11-09 00:47:24 +0100

    Completing main task (showing info from API)

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const getContainer = document.querySelector(".row");
+const getContainer = document.querySelectorAll(".row");
 
 const options = {
   method: "GET",
@@ -9,14 +9,14 @@ const options = {
 };
 
 fetch(
-  "https://striveschool-api.herokuapp.com/api/deezer/search?q=pink floyd",
+  "https://striveschool-api.herokuapp.com/api/deezer/search?q=rammstein",
   options
 )
   .then((response) => response.json())
-  .then((pinkfloyd) => {
-    console.log(pinkfloyd.data);
-    for (data of pinkfloyd.data) {
-      getContainer.innerHTML += `<div class="col-3">
+  .then((rammstein) => {
+    console.log(rammstein.data);
+    for (data of rammstein.data) {
+      getContainer[0].innerHTML += `<div class="col-3">
       <div class="card">
   <img src="${data.album.cover}" class="card-img-top" alt="...">
   <div class="card-body">
@@ -25,6 +25,48 @@ fetch(
   </div>
 </div>
       </div>`;
+    }
+  })
+  .catch((err) => console.error(err));
+
+fetch(
+  "https://striveschool-api.herokuapp.com/api/deezer/search?q=metallica",
+  options
+)
+  .then((response) => response.json())
+  .then((metallica) => {
+    console.log(metallica.data);
+    for (data of metallica.data) {
+      getContainer[1].innerHTML += `<div class="col-3">
+      <div class="card">
+  <img src="${data.album.cover}" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">${data.artist.name}</h5>
+    <p class="card-text">Title: ${data.title}</p>
+  </div>
+</div>
+      </div>`;
+    }
+  })
+  .catch((err) => console.error(err));
+
+fetch(
+  "https://striveschool-api.herokuapp.com/api/deezer/search?q=pink floyd",
+  options
+)
+  .then((response) => response.json())
+  .then((pink_floyd) => {
+    console.log(pink_floyd.data);
+    for (data of pink_floyd.data) {
+      getContainer[2].innerHTML += `<div class="col-3">
+        <div class="card">
+    <img src="${data.album.cover}" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">${data.artist.name}</h5>
+      <p class="card-text">Title: ${data.title}</p>
+    </div>
+  </div>
+        </div>`;
     }
   })
   .catch((err) => console.error(err));
